d83b466fa48e84ce30ae396b02fff8dd20b724dc
eloynashe12 <eloy.leiva.valderrama@alumnos.sip.cl>
2023-03-15 13:02:21 -0300

    commit

---
+++ PYTHON/Fundamentals/fundamentals/hola/Diccionarios.py
@@ -0,0 +1,3 @@
+else:
+
+print("Sentencia else final")
---
+++ PYTHON/Fundamentals/fundamentals/hola/bucles.py
@@ -0,0 +1,36 @@
+#
+#1
+for i in range(1,151):
+    print(i)
+
+#2
+for i in range(5,1005,5):
+    print(i)
+
+#3
+for i in range(1,101):
+    if i % 10 == 0:
+        print("coding dojo")
+    elif i % 5 == 0:
+        print("coding")
+    else: 
+        print(i)
+
+#4
+#("NUMEROS IMPARES") 
+for i in range(1,500000,2):
+    print(i)
+
+5#
+#( RESTA DE 4 en 4 )
+for i in range(2018,0,-4):
+    print(i)
+
+6#
+N1= 2
+N2= 9
+N3= 3
+
+for i in range(N1,N2+1):
+    if i % N3 == 0:
+        print(i)
