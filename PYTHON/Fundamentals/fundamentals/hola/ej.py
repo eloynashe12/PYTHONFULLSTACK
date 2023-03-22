4477fd68ae2695e31798b8ecbe0800b33f32e6de
eloynashe12 <eloy.leiva.valderrama@alumnos.sip.cl>
2023-03-22 11:59:04 -0300

    commit

--- PYTHON/Fundamentals/fundamentals/hola/ej.py
+++
@@ -1,7 +0,0 @@
-#3;longitud
-def longitud(lista):
-
-    return lista[0] + len(lista)
-
-
-print (longitud([1,2,3,4,5]))
---
+++ PYTHON/Fundamentals/fundamentals/hola/funciones_intermedias_1.py
@@ -0,0 +1,64 @@
+#1
+x = [ [5,2,3], [10,8,9] ] 
+estudiantes = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'futbol' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+#2
+x[1][0] = 15
+print(x)
+estudiantes[0]['last_name'] = "Bryant"
+print(estudiantes)
+
+directorio_deportes['futbol'][0] = 'Andres'
+print( directorio_deportes['futbol'])
+z[0]['y'] = 30
+print(z)
+
+
+#3
+estudiantes = [
+        {'first_name':  'Michael', 'last_name' : 'eloycito'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterate_dictionary(list):
+    for i in range(0, len(list)):
+        output = ""
+        for key,val in list[i].items():
+            output += f" {key} - {val},"
+        print(output)
+
+iterate_dictionary(estudiantes)
+    
+
+def iterate_dictionary2(key_name,list):
+    for i in range(0, len(list)):
+        
+        for key,val in list[i].items():
+            if key == key_name:
+                print(val)
+iterate_dictionary2('first_name',estudiantes)
+iterate_dictionary2('last_name',estudiantes)
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def print_info(dict):
+    for key,val in dict.items():
+        print("--------------")
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0, len(val)):
+            print(val[i])
+print_info(dojo)
+
